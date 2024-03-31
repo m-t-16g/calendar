2851f7ef65c48da12691a004852ca9c977f7cc3f
MasashiTSUGE <zhezhiyazhi@gmail.com>
2024-03-31 15:24:39 +0900

    ファイルの変更 readme追加

--- conection.php
+++ conection.php
@@ -1,8 +1,10 @@
 <?php
 const PDO_LOCALHOST = 'mysql:host=localhost;dbname=calendar;charset=utf8';
 const PDO_CONTAINER = 'mysql:host=db;dbname=calendar;chaeset=utf8';
+const PDO_STARSERVER = 'mysql:host=mysql1.php.starfree.ne.jp;dbname=mercy2g_db;charset=utf8';
 $errorlog = '';
-$container_log='';
+$container_log = '';
+$starserver_log = '';
 // ローカルホストに接続を試す
 try {
     // xamppとdockerでホストを変えないと接続できなかったため、それぞれの設定を定数として保存
@@ -18,7 +20,15 @@ if ($errorlog) {
         $container_log .= 'コンテナ―接続ログ' . $e;
     }
 }
-// 両方失敗していたらechoでエラーメッセージを返す
+// スターサーバの接続テスト
 if ($errorlog && $container_log) {
-    echo $errorlog;
+    try {
+        $pdo = new PDO(PDO_STARSERVER, 'mercy2g_cca', 'X9U5WC9SYnQ3YMc');
+    } catch (PDOException $e) {
+        $starserver_log .= 'スターサーバ接続ログ' . $e;
+    }
+}
+// 両方失敗していたらechoでエラーメッセージを返す
+if ($errorlog && $container_log && $starserver_log) {
+    echo $errorlog, $container_log, $starserver_log;
 }
