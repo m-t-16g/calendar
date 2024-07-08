dee1851f4fbaea08c99cf82e8098a2b2bf48cf8b
M.T <155932544+m-t-16g@users.noreply.github.com>
2024-07-08 11:15:07 +0900

    Update register.php

--- register.php
+++ register.php
@@ -29,26 +29,3 @@ if (is_null($id)) {
         echo $e;
     }
 }
-
-// 登録対象のidに予定が入ってないかチェック
-// $check = $pdo->prepare('SELECT * FROM events WHERE id=?');
-// $check->execute([$id]);
-// $result = $check->fetchAll();
-// // すでに予定が入っていない場合新規登録
-// if (empty($result)) {
-//     $post = $pdo->prepare('INSERT INTO events VALUES(?,?,?,?,?,?)');
-//     try {
-//         $post->execute([$id, $title, $date, $time, $category, $detail]);
-//     } catch (PDOException $e) {
-//         $res = $e;
-//         echo json_encode($res);
-//     }
-// } else {
-//     // 予定が入っている場所の場合内容の変更
-//     $change = $pdo->prepare('UPDATE events SET title=?, date=?, time=?, category =? , detail=? WHERE date=?');
-//     try {
-//         $change->execute([$title, $date, $time, $category, $detail, $date]);
-//     } catch (PDOException $e) {
-//         echo $e;
-//     }
-// }
